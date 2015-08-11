d8e400cb5d6564bce93799320088814a12be0900
Casey Clyde <caseysclyde@gmail.com>
2015-08-10 19:15:46 -0700

    codes

--- SlingShip - Unity/Assets/Code/Planet.js
+++ SlingShip - Unity/Assets/Code/Planet.js
@@ -1,9 +1,18 @@
 ﻿#pragma strict
 
-function Start () {
+var gravity : float = 20.0;
+var rotationSpeed : float = 10.0;
 
+var sun : GameObject;
+
+function Start () {
+	sun = GameObject.FindGameObjectWithTag("Sun");
 }
 
 function Update () {
+	
+}
 
+function FixedUpdate() {
+	transform.RotateAround (sun.transform.position, Vector3.forward, rotationSpeed * Time.deltaTime);
 }
