f7cb9bb4fac016fc650574d21b59f5cf47586f10
jesterswilde <jesterswilde@gmail.com>
2015-08-10 22:21:20 -0700

    Animte Blend script

---
+++ SlingShip - Unity/Assets/Code/AnimateBlend.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateBlend : MonoBehaviour {
+
+	[SerializeField]
+	Vector2 mainTexture;
+	[SerializeField]
+	Vector2 secondTexture;
+	[SerializeField]
+	Vector2 mask;
+
+	Vector2 mtStart;
+	Vector2 stStart;
+	Vector2 mStart; 
+
+	Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> (); 
+	}
+	
+	void Update(){
+		mtStart += mainTexture * Time.deltaTime; 
+		stStart += secondTexture * Time.deltaTime;
+		mStart += mask * Time.deltaTime ; 
+		rend.material.SetTextureOffset ("_MainTex", mtStart);
+		rend.material.SetTextureOffset ("_PathTex", stStart);
+		rend.material.SetTextureOffset ("_PathMask", mStart);
+	}
+}
