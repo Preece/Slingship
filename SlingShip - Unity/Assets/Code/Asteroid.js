bf2b53e20fcac7ceb843e99f68109d4fbce97170
Casey Clyde <caseysclyde@gmail.com>
2015-08-12 13:08:00 -0700

    ass teroids n hemmrhoids

---
+++ SlingShip - Unity/Assets/Code/Asteroid.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+var ship : GameObject;
+
+function Start () {
+	ship = GameObject.FindGameObjectWithTag("Ship");
+}
+
+function Update () {
+
+}
+
+function OnCollisionEnter(collision: Collision) {
+	
+	//ship.GetComponent(Ship).modifyVelocity(collision.relativeVelocity * -100);
+	
+	for (var contact : ContactPoint in collision.contacts) {
+		print(contact.thisCollider.name + " hit " + contact.otherCollider.name);
+		Debug.Log(contact.point);
+		
+		var vecLine = transform.position - contact.point;
+		
+		ship.GetComponent(Ship).modifyVelocity(vecLine * -200);
+		
+		gameObject.GetComponent(Rigidbody).velocity = vecLine * 1;
+	};
+	
+	
+	
+	//Destroy(gameObject);
+}
